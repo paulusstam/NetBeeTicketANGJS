6150f94ec9da25f3ef77a66be969448f4057b8dd
Pavlos Stam <pstamat@outlook.com>
2017-06-26 17:50:41 +0300

    data bindings

--- NetBeeTicketANGJS/App/CustomerForm/ntController.js
+++ NetBeeTicketANGJS/App/CustomerForm/ntController.js
@@ -1,4 +1,16 @@
 ﻿NetBeeTicketApp.controller('ntController',
     function ntController($scope, ntService) {
         $scope.customer = ntService.customer;
+
+        $scope.subscriptionType = [
+            "Monthly",
+            "Quarterly",
+            "Semi-Annual",
+            "Annual"
+        ];
+
+        $scope.submitForm = function () {
+
+        }
+
     });
